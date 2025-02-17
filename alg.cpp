f35a54b99f587dce0acd9d7a7b8a4505af0a6be7
zed932 <mescheryakovsrg@gmail.com>
2025-02-17 21:21:31 +0300

    Add files via upload

--- alg.cpp
+++ alg.cpp
@@ -1,13 +1,19 @@
+// Copyright 2021 NNTU-CS
+
 int gcd(int a, int b) {
-  if (a == 0) {return b;}
-  if (b == 0) {return a;}
+  if (a == 0) {
+    return b;
+  }
+  if (b == 0) {
+    return a;
+  }
+  
   while (a != b) {
     if (a > b) {
       a = a - b;
-      }
-    else {
+      } else {
       b = b - a;
+      }
     }
-  }
   return a;
 }
